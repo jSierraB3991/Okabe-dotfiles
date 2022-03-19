fd96c280be584a3c8896bab97dd817d2839b0aa9
Okabe Rintarou <eliotandelon@gmail.com>
2022-03-19 02:38:48 -0500

    remove config unnecesary

--- i3/scripts/ethernet_status.sh
+++
@@ -1,3 +0,0 @@
-#! /bin/bash
- 
-echo "$(curl --silent https://whatismyipaddress.com/es/mi-ip | grep 'Your IP'  | grep -Eo '[0-9.]+' | tail -1)"
--- install-on-python.sh
+++
@@ -1,8 +0,0 @@
-python -m ensurepip --upgrade
-
-#https://pip.pypa.io/en/stable/installation/
-curl https://bootstrap.pypa.io/get-pip.py --output pip.py
-python get-pip.py
-
-#Update
-python -m pip install --upgrade pip
