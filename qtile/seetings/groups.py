ad6245286be0af026980a957dfaada7569284a44
Juan David Sierra <eliotandelon@gmail.com>
2022-07-01 05:35:00 -0500

    change icon of android for icon intellij

--- qtile/seetings/groups.py
+++ qtile/seetings/groups.py
@@ -8,7 +8,7 @@ from .keys import mod, keys
 # Get the icons at https://www.nerdfonts.com/cheat-sheet (you need a Nerd Font)
 
 groups = [Group(i) for i in [
-    "  ",  "   ", "  ", "  ", "  ", " 6 ", " 7 ", " 8 ", " 9 "
+    "  ",  "   ", "  ", "  ", "  ", " 6 ", " 7 ", " 8 ", " 9 "
 ]]
 
 for i, group in enumerate(groups):
